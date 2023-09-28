798bbdf97b406b425a5ff012ff425ff9037d5d6a
Dumitru Frimu <dumitrufrimu118@gmail.com>
2023-09-28 12:13:55 +0300

    Da

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,33 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-
+public class conversie_b_10 {
     public static void main(String[] args) {
-        Scanner cin = new Scanner(System.in);
+        try (Scanner cin = new Scanner(new File("conversie_b_10.in"));
+             PrintWriter out = new PrintWriter(new File("conversie_b_10.out"))) {
 
-        int n = cin.nextInt();
-        int contor = 0;
+            int numar, baza, cnt = 0, rezultat = 0, j = 0;
+            int[] n = new int[10];
 
-        int[] vec1 = new int[1001];
+            numar = cin.nextInt();
+            baza = cin.nextInt();
 
-        System.out.println();
+            while (numar != 0) {
+                n[++cnt] = numar % 10;
+                numar /= 10;
+            }
 
-        for (int i = 1; i <= n; i++){
-            vec1[i] = cin.nextInt();
+            int[] rezerva = new int[10];
+
+            for (int i = cnt; i >= 1; i--)
+                rezerva[++j] = n[i];
+
+            for (int i = 1; i <= j; i++)
+                rezultat += rezerva[i] * Math.pow(baza, j - i);
+
+            out.println(rezultat);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-g
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                for (int l = 1; l <= n; l++) {
-                    if ((vec1[i] % 5 == 0 || vec1[j] % 5 == 0 || vec1[l] % 5 == 0) && l > j && j > i)
-                        contor++;
-                }
-
-            System.out.print(" " + contor);
-
     }
 }
